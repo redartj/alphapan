2a6911c6559032be9718d472170b988fad221f10
redartj <redartj@gmail.com>
2023-01-10 22:15:59 +0900

    first chart

--- jboard/views.py
+++ jboard/views.py
@@ -9,18 +9,6 @@ from plotly.offline import plot
 import plotly.express as px
 
 
-# inputting data
-def datainput(request):
-    # csv에서 불러오기
-
-    # 값 뽑아서 변수에 넣음
-
-
-    # 모델에 값 넣기
-
-
-    return HttpResponse("Done")
-
 # test
 def index(request):
     return HttpResponse('Hello!!')
@@ -32,10 +20,42 @@ class HomeView(TemplateView):
 
 class DailyView(TemplateView):
     template_name = 'jboard/daily.html'
+    def get_context_data(self, **kwargs):
+        qs = ExRateUSDKRW.objects.all()
+        start = self.request.GET.get('start')
+        end = self.request.GET.get('end')
+        if start:
+            qs = qs.filter(date__gte=start)
+        if end:
+            qs = qs.filter(date__lte=end)
 
+        fig = px.line(
+            x=[r.date for r in qs],
+            y=[r.lastvalue for r in qs],
+            title='원달러 환율',
+            labels={'x':'일자','y':'환율'}
+        )
+        fig.update_layout(title={
+            'font_size':22,
+            'xanchor':'center',
+            'x':0.5
+        })
+        fig.update_xaxes(
+            #dtick="M1",
+            minor=dict(ticks="inside", showgrid=True),
+            tickformat="%m\n%Y")
+
+        chart = fig.to_html()
+        context = super(DailyView, self).get_context_data(**kwargs)
+        context['ExRateUSDKRW']=ExRateUSDKRW.objects.all()
+        context['chart']=chart
+        return context
 
 class MacroView(TemplateView):
     template_name = 'jboard/macro.html'
+
+class datainput(TemplateView):
+    template_name = 'jboard/datainput.html'
 
 
 class PortfolioView(TemplateView):
